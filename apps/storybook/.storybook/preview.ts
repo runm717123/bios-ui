4b8f198346f51fce59e3a96e2fcb39bb7362163b
runm717123 <runm717123@gmail.com>
2025-05-25 11:44:37 +0700

    add base story styles

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,15 +1,24 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from '@storybook/react';
 import '@bios-ui/core';
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+		backgrounds: {
+			default: 'custom',
+			values: [
+				{
+					name: 'custom',
+					value: 'var(--bg-main)',
+				},
+			],
+		},
+	},
 };
 
 export default preview;
