30dd119903b7f1919a92a2e6168c96c294ed8cf8
runm717123 <runm717123@gmail.com>
2025-05-25 18:43:36 +0700

    feat(button): add hover and active style

--- packages/basic/src/styles.ts
+++ packages/basic/src/styles.ts
@@ -5,4 +5,16 @@ export const bButton = css`
 	color: var(--color-foreground);
 	border: 2px solid var(--color-foreground);
 	padding: 0.5rem 1rem;
+
+	&:hover {
+		box-shadow:
+			0 0 5px 0 var(--color-foreground) inset,
+			0 0 10px 2px var(--color-foreground);
+	}
+
+	&:active {
+		background-color: white;
+		border-color: white;
+		box-shadow: none;
+	}
 `;
