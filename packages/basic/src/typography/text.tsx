a003967cf654ee9ac2083e553aa6457308ceb2f2
runm717123 <runm717123@gmail.com>
2025-06-05 23:04:56 +0700

    chore(core): remove unused assets, use cnd fonts

--- packages/basic/src/typography/text.tsx
+++ packages/basic/src/typography/text.tsx
@@ -3,3 +3,7 @@ import { styled } from '@linaria/react';
 export const BodyText = styled.span`
 	font-size: var(--font-size-md);
 `;
+
+export const BodyTextSmall = styled.p`
+	font-size: var(--font-size-sm);
+`;
