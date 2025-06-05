10b809af9bb0d8ceb01782054388730ceb786221
runm717123 <runm717123@gmail.com>
2025-06-05 22:28:08 +0700

    feat(core): add size style

--- packages/core/src/utils/size.js
+++ packages/core/src/utils/size.js
@@ -3,3 +3,23 @@ import { css } from "@linaria/core";
 export const bWFit = css`
   width: fit-content;
 `;
+
+export const bWFull = css`
+  width: 100%;
+`;
+
+export const bWAuto = css`
+  width: auto;
+`;
+
+export const bHFit = css`
+  height: fit-content;
+`;
+
+export const bHFull = css`
+  height: 100%;
+`;
+
+export const bHAuto = css`
+  height: auto;
+`;
