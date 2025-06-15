6600182f330558043c56998e774b005d65b6ba29
runm717123 <runm717123@gmail.com>
2025-06-15 18:19:13 +0700

    chore(basic): fix build to include necessary file

--- packages/basic/vite.config.ts
+++ packages/basic/vite.config.ts
@@ -5,7 +5,6 @@ import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-// get current dirname
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
@@ -19,15 +18,25 @@ export default defineConfig({
 		}),
 		dts({
 			include: ['src'],
+			outDir: 'dist/types',
+			insertTypesEntry: true,
 		}),
 	],
 	build: {
+		outDir: 'dist',
+		target: 'esnext',
 		lib: {
-			entry: resolve(__dirname, 'src/index.ts'),
+			entry: resolve(__dirname, 'src', 'index.ts'),
 			name: 'BiosUI',
-			// the proper extensions will be added
-			fileName: 'bios-ui',
 			formats: ['es'],
+		},
+		rollupOptions: {
+			external: ['@linaria/react', '@linaria/core', 'react/jsx-runtime', 'react/jsx-dev-runtime'],
+			output: {
+				preserveModules: true,
+				preserveModulesRoot: 'src',
+				entryFileNames: '[name].js',
+			},
 		},
 	},
 });
