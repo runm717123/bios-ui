576bdda8f3775603eff1ac0249395e6658148a07
runm717123 <runm717123@gmail.com>
2025-06-27 12:36:07 +0700

    feat: svelte-v2

---
+++ packages/svelte-v2/src/lib/Button.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	export let size: 'small' | 'medium' | 'large' = 'medium';
+	export let className: string = '';
+</script>
+
+<button class={`bButton ${className}`} data-size={size} {...$$restProps}>
+	<slot />
+</button>
+
+
+<style>
+	.bButton {
+		background-color: transparent;
+		color: var(--color-fg-dark);
+		border: 2px solid var(--color-fg-dark);
+	}
+
+	.bButton[data-size='small'] {
+		padding: 0.25rem 0.5rem;
+		font-size: var(--font-size-sm);
+	}
+
+	.bButton[data-size='medium'] {
+		padding: 0.5rem 1rem;
+		font-size: var(--font-size-md);
+	}
+
+	.bButton[data-size='large'] {
+		padding: 0.75rem 1.5rem;
+		font-size: var(--font-size-lg);
+	}
+
+	.bButton:hover {
+		box-shadow:
+			0 0 5px 0 var(--color-fg-dark) inset,
+			0 0 10px 2px var(--color-fg-dark);
+	}
+
+	.bButton:active {
+		background-color: rgba(255, 255, 255, 0.9);
+		border-color: white;
+		box-shadow: none;
+	}
+</style>
---
+++ packages/svelte-v2/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import "@bios-ui/core";
+	import Button from '$lib/Button.svelte';
+</script>
+
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Button onClick={() => alert('Button clicked!')}>Click Me</Button>
