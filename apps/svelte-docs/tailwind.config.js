f848002a4b4fcd7a840a080e37e0a42b1e85ff85
runm717123 <runm717123@gmail.com>
2025-06-29 13:54:58 +0700

    feat(svelte-docs): add tailwind.config

---
+++ apps/svelte-docs/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.ts
+export default {
+  content: [
+    './src/**/*.{html,svelte,ts,js}',
+    './node_modules/@bios-ui/**/*.{svelte}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
